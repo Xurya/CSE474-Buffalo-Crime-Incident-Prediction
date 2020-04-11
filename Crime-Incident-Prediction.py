278174b1d6c60072a323e9fb5e4d644c624bf5db
Xurya <xur1@bxscience.edu>
2020-04-11 15:16:47 -0400

    #1 partitioning

--- Crime-Incident-Prediction.py
+++ Crime-Incident-Prediction.py
@@ -10,10 +10,25 @@ def main():
         reader = csv.reader(f)
         sort = sorted(reader, key=lambda row: sort_by_date(row[2]))
 
-    with open("Crime_Incidents_Sorted.csv", 'w', newline='') as f:
-        writer = csv.writer(f)
-        for line in sort:
-            writer.writerow(line)
+    # Writes to a single, sorted file. Previously tested and now commented out for reference
+    # with open("Crime_Incidents_Sorted.csv", 'w', newline='') as f:
+    #     writer = csv.writer(f)
+    #     for line in sort:
+    #         writer.writerow(line)
+
+    partitions = list(partition(sort, 100))
+    fileCount = 0;
+    for lst in partitions:
+        with open("Crime_Incidents_Sorted" + str(fileCount) + ".csv", 'w', newline='') as f:
+            writer = csv.writer(f)
+            for line in lst:
+                writer.writerow(line)
+        fileCount += 1
+
+
+def partition(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 
 def sort_by_date(date):
