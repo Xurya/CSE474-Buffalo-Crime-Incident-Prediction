ba8e020104bf83efd53d3ec727b1398f67dc1ea5
Xurya <xur1@bxscience.edu>
2020-04-11 14:07:04 -0400

    #1

--- Crime-Incident-Prediction.py
+++ Crime-Incident-Prediction.py
@@ -1,10 +1,14 @@
 import csv
+import operator
+from datetime import datetime
 
 def main():
     #Take the files and sort them by date to create new sorted csvs
     with open("Crime_Incidents.csv") as f:
         reader = csv.reader(f)
-
+        sort = sorted(reader, key=lambda row: datetime.strptime(row[2], "%m/%d/%y %H:%M:%S %p"))
+        for line in sort:
+            print(line)
 
 if __name__ == "__main__":
     main()
